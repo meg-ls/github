3cc50c1e8e4f21c9851693eca93d0712370f762e
Megan Steele <megan.steele@annalise.ai>
2024-01-09 18:19:12 +1000

    set up info

--- git_test.R
+++ git_test.R
@@ -2,3 +2,18 @@
 get_yesterday <- function(){
   return(Sys.Date() - 1)
 }
+
+gitcreds::gitcreds_set()
+
+
+library(usethis)
+
+# Add PAT to enviro file and save
+edit_r_environ()
+
+# Restart R
+
+# Reload usethis library
+library(usethis)
+
+use_github(protocol = "https", auth_token = Sys.getenv("GITHUB_PAT"))
